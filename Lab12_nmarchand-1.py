7d20cec699bbfecb56dfd6a05d4fde6e9761e164
nmarchand200728-maker <nmarchand200728@gmail.com>
2025-11-17 18:15:11 -0500

    Update Lab12_nmarchand-1.py

--- Lab12_nmarchand-1.py
+++ Lab12_nmarchand-1.py
@@ -48,10 +48,10 @@ class AlienInvasion:
     
     def _check_keydown_events(self, event):
          """Respond to keypresses."""
-         if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-         elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+         if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+         elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
          elif event.key == pygame.K_q:
               sys.exit() 
          elif event.key == pygame.K_SPACE:
@@ -59,10 +59,10 @@ class AlienInvasion:
 
     def _check_keyup_events(self, event):
          """Respond to key releases."""
-         if event.key == pygame.K_RIGHT:
-              self.ship.moving_right = False
-         elif event.key == pygame.K_LEFT:
-             self.ship.moving_left = False              
+         if event.key == pygame.K_UP:
+              self.ship.moving_up = False
+         elif event.key == pygame.K_DOWN:
+             self.ship.moving_down = False              
     def _fire_bullet(self):
          """Create a new bullet and add it to the bullet group."""
          if len(self.bullets) < self.settings.bullets_allowed:
@@ -94,3 +94,4 @@ if __name__ == "__main__":
 
     ai = AlienInvasion()
     ai.run_game()
+
